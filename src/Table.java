ddea0271247cde23efa65624af8790b6f75a1756
Vladislav Skachinsky <vskachinskyr@gmail.com>
2021-10-28 23:23:27 +0300

    upd

--- src/Table.java
+++ src/Table.java
@@ -13,9 +13,9 @@ public class Table {
 
     public void drawTable() {
         String[][] data = new String[options.size() +1][options.size() +1];
-        for (int i = 0, m = 1; i < options.size(); i++, m++, n++) {
-            for (int j = 0; j < options.size(); j++) {
-                data[m][j] = options.get(j);
+        for (int i = 0, m = 1; i < options.size(); i++, m++) {
+            for (int j = 0, n = 1; j < options.size(); j++, n++) {
+                data[m][n] = options.get(j);
             }
         }
 
